fddbcb0b0dc0a9849ee3bafa84fca463377904a0
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-08-25 19:29:13 -0300

    Update Question.py

---
+++ Binary Search/Question.py
@@ -0,0 +1,12 @@
+data = [20, 19 , 15 , 12, 8 , 4, 2, 0]
+query = 8
+
+
+def locate_cards(cards, query):
+    for i in range(len(cards)):
+        if cards[i] == query :
+            print (cards[i])
+            
+            
+locate_cards(data, query)
+            
