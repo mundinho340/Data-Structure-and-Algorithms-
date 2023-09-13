51ad8b011be0d5aea47b1655c68bf31c65b8b39c
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-09-13 17:07:35 -0300

    Create 00Vetor.py

---
+++ bigO/00Vetor.py
@@ -0,0 +1,11 @@
+#Aqui temos um exemplo de uma função linear
+##Uma função linear que o seu tempo de execução depende da entrada e quanto maior for a entrada maior sera o tempo de execução
+
+def square(n):
+    i =0
+    while i< n :
+        s= 10 * 50
+        i+=1
+        print (s)
+
+square(5)
