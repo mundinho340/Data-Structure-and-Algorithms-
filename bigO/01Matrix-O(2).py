9950d3b25b98a51ec3d0ca4fe2b44e64865c7866
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-10-01 16:20:53 -0300

    Create 01Matrix-O(2).py

---
+++ bigO/01Matrix-O(2).py
@@ -0,0 +1,11 @@
+# simple example o(n2)
+def quadrado (n):
+    i=0
+    while i< n:
+        j=0
+        print("")
+        while j<n :
+            print (f'{j}')
+            j+=1
+        i+=1
+quadrado(4)
