f9eb5073872c30abdcb17b2bee0f58dd9c25b986
mundinho340 <raimundodatlb@gmail.com>
2023-12-05 09:39:15 +0200

    hash_function

--- HashTables/Example/ex2.py
+++ HashTables/Example/ex2.py
@@ -2,3 +2,6 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [None]* size
+    
+    def hash_function(self, key):
+        return hash(key) % self.size
